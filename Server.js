734715035d033246fc157e2aa9a57b17d6d33ddc
InnovateBham_7 <InnovateBham_7@administartor.local>
2017-08-03 16:45:34 -0500

    commit2

--- Server.js
+++ Server.js
@@ -1,59 +1,3 @@
-// var http = require('http');
-// var url = require('url');
-// var fs = require('fs');
-// var path = require('path');
-// var jsonPath = path.join(__dirname, 'data.json');
-// console.log('starting server');
-// http.createServer(function (req, res) {
-//     var parsedUrl = url.parse(req.url);
-//     if (parsedUrl.pathname === '/chirps' && req.method === 'GET') {
-       
-//         fs.readFile(jsonPath, function (err, file){
-//             if (err){
-//             res.writeHead(500);
-//             res.end('File could not be read');
-//             }
-//         res.write(file);
-//         res.end();
-//         });  
-//     }
-//     else if (parsedUrl.pathname === '/chirps' && req.method === 'POST'){
-//         var chunks= '', 
-//         data;
-
-//         req.on('data', function(chunk){
-//             chunks += chunk;
-//             if(chunks.length>1e6){
-//                 requ.connection.destroy();
-//             }
-//                 data = JSON.parse(chunks);
-               
-//         });
-
-//           fs.readFile(jsonPath, 'utf-8', function(err, file) {
-//             if (err) {
-//                 res.writeHead(500);
-//                 res.end('Could not read file');
-//             }
-
-//             var arr = JSON.parse(file);
-
-//                 arr.push(data.chirp);
-
-//             fs.writeFile(jsonPath, JSON.stringify(arr), function(err, success) {
-//                 if (err) {
-//                     res.writeHead(500);
-//                     res.end('Couldn\'t successfull store data');
-//                 } else {
-//                     res.writeHead(201, 'Created');
-//                     res.end(JSON.stringify(arr));
-//                 }
-//             });
-//         });
-//     }
-// })
-// .listen(3000);
-
 var http = require('http');
 var url = require('url');
 var fs = require('fs');
@@ -61,12 +5,38 @@ var path = require('path');
 var randomID = require("random-id");
 var ID = randomID(8, '0')
 var jsonPath = path.join(__dirname, 'data.json');
-
 console.log('starting server');
-http.createServer(function(req, res) {
-    var parsedUrl = url.parse(req.url);
+console.log(Location)
 
-    if (parsedUrl.pathname === '/chirps' && req.method === 'GET') {
+http.createServer(function(req, res) {
+var parsedUrl = url.parse(req.url);
+var method=req.method
+// console.log(method)
+
+if (parsedUrl.pathname == '/chirps/' && req.method === 'DELETE'){
+    fs.readFile(jsonPath, 'utf-8', function(err, file) {
+            if (err) {
+                res.writeHead(500);
+                res.end('Could not read file');
+            }
+
+            var arr = JSON.parse(file);
+        for(i=0; i<arr.length; i++){
+            var idURL= arr[1].id
+            console.log( req.body.data)
+            console.log(parsedUrl.pathname + idURL)
+            // if(arr[i].id == parsedUrl.pathname + id){
+            //     delete(arr[i])
+            // }
+        }
+            // arr.push(data);
+            // arr.push(data.chirp);
+            //  console.log(arr)
+            
+        });
+                
+        }
+    else if (parsedUrl.pathname === '/chirps' && req.method === 'GET') {
         // fs.createReadStream(jsonPath)
         // .on('error', function(err) {
         //     res.writeHead(500);
@@ -82,24 +52,21 @@ http.createServer(function(req, res) {
 
             res.write(file);
             res.end();
+          console.log(  req.params)
         });
     }
-        else if (parsedUrl.pathname === '/chirps/one/'+ idUrl && req.method === 'DELETE'){
-                console.log(id)
-            
-            var idUrl=data.id
-        }
+        
      else if (parsedUrl.pathname === '/chirps' && req.method === 'POST') {
         var chunks = '',
             data;
-
+            console.log()
         req.on('data', function(chunk) {
             chunks += chunk;
 
             if (chunks.length > 1e6) {
                 req.connection.destroy();
             }
-            console.log(data)
+            // console.log(data)
             data = JSON.parse(chunks);
         });
 
@@ -110,7 +77,7 @@ http.createServer(function(req, res) {
             }
 
             var arr = JSON.parse(file);
-            data.id= ID
+            data.id = ID
         var c = data
             arr.push(data);
             // arr.push(data.chirp);
@@ -125,6 +92,8 @@ http.createServer(function(req, res) {
                 }
             });
         });
+        
     }
+    
 })
 .listen(3000);
